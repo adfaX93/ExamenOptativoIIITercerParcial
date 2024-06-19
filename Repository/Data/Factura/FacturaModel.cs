b12d96a762e5ddc4c1425caeec3bcbfc3d186184
Alex Fretes <67652643+adfaX93@users.noreply.github.com>
2024-06-19 12:57:47 -0400

    se crea FacturaModel

---
+++ Repository/Data/Factura/FacturaModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Data.Factura
+{
+    public class FacturaModel
+    {
+        public int Id { get; set; }
+        public int Id_Cliente { get; set; }
+        public int Id_Sucursal { get; set; }
+        public string Nro_Factura { get; set; }
+        public string Fecha_Hora { get; set; }
+        public double Total { get; set; }
+        public double Total_iva5 { get; set; }
+        public double Total_iva10 { get; set; }
+        public double Total_iva { get; set; }
+        public string Total_Letras { get; set; }
+    }
+}
+}
