6b8f3d44e21df0273e3369605ed56a58f57021c0
Alex Fretes <67652643+adfaX93@users.noreply.github.com>
2024-06-19 09:19:39 -0400

    se crea la interfaz IClienteRespository

---
+++ Repository/Data/Cliente/IClienteRepository.cs
@@ -0,0 +1,17 @@
+﻿using Repository.Data.Sucursal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Data.Cliente
+{
+    public interface IClienteRepository
+    {
+        bool add(ClienteModel clienteModel);
+        bool update(ClienteModel clienteModel);
+        bool delete(int id);
+        IEnumerable<ClienteModel> GetAll();
+    }
+}
--- Repository/Data/Sucursal/ISucursalRepository.cs
+++ Repository/Data/Sucursal/ISucursalRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Repository.Data.Sucursal
 {
-    internal interface ISucursalRepository
+    public interface ISucursalRepository
     {
         bool add(SucursalModel sucursalModel);
         bool update(SucursalModel sucursalModel);
