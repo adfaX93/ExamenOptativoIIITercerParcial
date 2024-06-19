06dae35bf17ce43fbf4ccc14ea820b71a1457a78
Alex Fretes <67652643+adfaX93@users.noreply.github.com>
2024-06-19 13:08:07 -0400

    se crea IFacturaRepository

---
+++ Repository/Data/Factura/IFacturaRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Data.Factura
+{
+    public interface IFacturaRepository
+    {
+        bool add(FacturaModel factura);
+        bool update(FacturaModel factura);
+        bool delete(int id);
+        FacturaModel getById(int id);
+        IEnumerable<FacturaModel> GetAll();
+    }
+}
