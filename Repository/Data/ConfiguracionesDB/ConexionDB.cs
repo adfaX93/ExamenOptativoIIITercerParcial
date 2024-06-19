80f38d4c39e1d849ced50695ec9a9652f9c46bfe
Alex Fretes <67652643+adfaX93@users.noreply.github.com>
2024-06-18 22:58:31 -0400

    Add project files.

---
+++ Repository/Data/ConfiguracionesDB/ConexionDB.cs
@@ -0,0 +1,26 @@
+﻿using Npgsql;
+
+namespace Repository.Data.ConfiguracionesDB
+{
+    public class ConexionDB
+    {
+        private string conexionString;
+        public ConexionDB(string conexionString)
+        {
+            this.conexionString = conexionString;
+        }
+        public NpgsqlConnection OpenConnection()
+        {
+            try
+            {
+                var conn = new NpgsqlConnection(conexionString);
+                conn.Open();
+                return conn;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
